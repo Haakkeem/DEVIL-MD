1da702c44a011c42e963f2598e21c0f3495219cc
Haakkeem <155872222+Haakkeem@users.noreply.github.com>
2024-06-27 16:09:56 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'public',
   OWNER_NAME: process.env.OWNER_NAME || "WASI-SER",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "923192173398",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2349138441084",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
